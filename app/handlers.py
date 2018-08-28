05a133c2d750ac26134f213129ed7dccfc4469a3
Lu Hong <luhong@amazon.com>
2018-08-27 18:20:56 -0700

    Fix typo in handlers.py

--- app/handlers.py
+++ app/handlers.py
@@ -6,7 +6,7 @@ import log_helper
 LOGGER = log_helper.getLogger(__name__)
 
 
-def handle_requeset(request, context):
+def handle_request(request, context):
     """Publish to Serverless Apps Repo.
 
     CodePipeline invokes the lambda to publish an application
